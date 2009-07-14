40b78b598b581784681512f956c825e84f12477a
jjhageman <jjhageman@gmail.com>
2009-07-14 00:36:01 -0700

    Added friendly_id to catagory model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,22 @@
 class CategoriesController < ApplicationController
+  
+  before_filter :ensure_current_category_url, :only => :show
+  
   def index
     @categories = Category.find(:all)
   end
 
   def show
-    @category = Category.find(params[:id])
+    redirect_to @category, :status => 301 if @category.has_better_id?
     @remaining_categories = Category.find(:all, :conditions => "id != #{@category.id}")
     @pranks = @category.pranks.paginate :per_page => 15, :order => 'rating_count DESC', :page => params[:page]
   end
+  
+  private
+  
+  def ensure_current_category_url
+    @category = Category.find(params[:id])
+    redirect_to @category, :status => :moved_permanently if @category.has_better_id?
+  end
+  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :pranks
   validates_presence_of :name
+  has_friendly_id :name, :use_slug => true
 end
